837b8822e51ceabc8ff3d5be81a43b3829b8eef6
malv007 <36649875+malv007@users.noreply.github.com>
2020-03-31 08:52:46 -0700

    Update es_stopwords.py

--- core/corefgraph/resources/dictionaries/es_stopwords.py
+++ core/corefgraph/resources/dictionaries/es_stopwords.py
@@ -5,6 +5,8 @@ __author__ = 'Rodrigo Agerri <rodrigo.agerri@ehu.es>'
 
 stop_words = set(( 'unas', 'una', 'unos', 'un', 'del', 'al', 'el', 'la', 'los', 'lo', 'las', 'de', 'en', 'sobre', 'por', 'dentro', 'hacia', 'desde', 'fuera', 'como', 'así', 'tal', 'o', 'y', 'esos', 'esas', 'este', 'esta', 'aquellas', 'aquellos', 'ese', 'esa', 'para', ',', 'es', 'fue', 'era', 'soy', 'eres', 'sido', 'eras'))
 
+extended_stop_words = set(())
+
 non_words = ('ejem', 'ajá', 'hm', 'jo')
 invalid_stop_words = ("SA", "SL", "etc", "hm")
 
